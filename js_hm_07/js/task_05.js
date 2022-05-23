c8c6f8a152457eb6283ed0089f4e2c1653ab0b7d
K1lltaN <94398731+K1lltaN@users.noreply.github.com>
2022-05-23 14:47:05 +0300

    Delete js_hm_07 directory

--- js_hm_07/js/task_05.js
+++
@@ -1,6 +0,0 @@
-let inputName = document.querySelector('#name-input');
-let spanText = document.querySelector('#name-output');
-
-inputName.addEventListener('input', (event) => {
-  event.target.value ? spanText.textContent = event.target.value : spanText.textContent = 'незнакомец'
-})
